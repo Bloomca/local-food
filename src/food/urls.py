7da3a87974514006bddef68416064ecb11e903da
Seva Zaikov <seva.zaikov@gmail.com>
2018-10-27 00:23:58 -0700

    add basic frame for food page

--- src/food/urls.py
+++ src/food/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, food
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('food/<food_slug>', food)
 ]
--- src/food/views.py
+++ src/food/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Country
+from .models import Country, Food, Origin
 
 def index(request):
   countries = Country.objects.all()
@@ -8,6 +8,17 @@ def index(request):
   context = {'countries': countries}
   return render(request, 'food/index.html', context=context)
 
-# def food(request, food_id):
+def food(request, food_slug):
+  food = Food.objects.get(slug=food_slug)
+  countries_list = []
+
+  countries = food.country_set.all()
+  for country in countries:
+    origin = Origin.objects.get(food=food, country=country)
+    countries_list.append((country, origin))
+
+  context = {'food': food, 'countries_list': countries_list}
+  return render(request, 'food/food.html', context=context)
+
 
 
