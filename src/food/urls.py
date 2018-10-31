b712417a71409e9c948543ec7fef564285aef801
Seva Zaikov <seva.zaikov@gmail.com>
2018-10-31 00:25:13 -0700

    add about page

--- src/food/urls.py
+++ src/food/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import index, food, country, countries, food_list
+from .views import index, food, country, countries, food_list, about
 
 urlpatterns = [
     path('', index),
     path('food/<food_slug>', food),
     path('country/<country_code>', country),
     path('food', food_list),
-    path('countries', countries)
+    path('countries', countries),
+    path('about', about)
 ]
