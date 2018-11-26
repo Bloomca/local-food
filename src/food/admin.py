13f61ac3d62aab9934773bdd9d995cee96dee219
Seva Zaikov <seva.zaikov@gmail.com>
2018-11-25 18:47:54 -0800

    add migration for food suggestions

--- src/food/admin.py
+++ src/food/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Country, Food, Origin
+from .models import Country, Food, Origin, FoodSuggestion
 
 # Register your models here.
 
 admin.site.register(Country)
 admin.site.register(Food)
 admin.site.register(Origin)
+admin.site.register(FoodSuggestion)
